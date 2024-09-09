dc3dec5f0b4c2bd0b30001bf63aabf92debf3bd6
ILya Cyclone <ilya.cyclone@gmail.com>
2024-09-10 01:22:16 +0400

    better CLI solve output

--- src/main/java/cyclone/games/watersortpuzzle/solver/cli/CLIActionSolve.java
+++ src/main/java/cyclone/games/watersortpuzzle/solver/cli/CLIActionSolve.java
@@ -3,6 +3,9 @@ package cyclone.games.watersortpuzzle.solver.cli;
 import cyclone.games.watersortpuzzle.solver.*;
 import org.apache.commons.cli.CommandLine;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CLIActionSolve implements CLIAction {
 
     private final TypedParser typedParser;
@@ -17,14 +20,23 @@ public class CLIActionSolve implements CLIAction {
         WinCondition winCondition = typedParser.getWinCondition(cmd);
 
         Puzzle puzzle = new Puzzle(tubes, winCondition);
-
         Solver solver = typedParser.getSolver(cmd);
+
+        TubesFormatter tubesFormatter = typedParser.parseTubesFormatter(cmd);
+        String puzzleFormatted = tubesFormatter.format(puzzle.tubes());
+        System.out.println(puzzleFormatted);
+        System.out.println("Looking for solution...");
+
         Solution solution = solver.solve(puzzle);
 
         if (solution != null) {
-            TubesFormatter tubesFormatter = typedParser.parseTubesFormatter(cmd);
+            List<int[]> moves = solution.moves();
+            System.out.println("Best solution in " + moves.size() + " moves: [" +
+                    moves.stream().map(move -> (move[0] + 1) + "-" + (move[1] + 1))
+                            .collect(Collectors.joining(", ")) + ']');
+
             Replay replay = new Replay(tubesFormatter);
-            String replayOutput = replay.replay(puzzle, solution.moves());
+            String replayOutput = replay.replay(puzzle, moves);
             System.out.println(replayOutput);
         } else {
             System.out.println("SOLUTION NOT FOUND");
